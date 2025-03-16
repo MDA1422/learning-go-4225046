0017ed4f724b052af75f99b583cf5a40beba200b
M. Daahir <71685940+MDA1422@users.noreply.github.com>
2025-03-16 17:31:04 +0000

    update

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Math")
+	i1,i2,i3:=20,40,60
+	intsum := i1+i2+i3
+	
+
+	fmt.Println("Sum:", intsum)
+
+	m1,m2,m3:=2,2,2
+	mprod := m1*m2*m3
+	
+
+	fmt.Println("mprod:", mprod)
 }
